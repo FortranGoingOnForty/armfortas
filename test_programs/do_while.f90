dfed961bea7c3ccddddeb2aecafda174fbee1bd1
mfwolffe <wolffemf@dukes.jmu.edu>
2026-04-04 08:42:29 -0400

    add control flow test programs
    do_while.f90, negative_step.f90, exit_cycle.f90, select_case.f90,
    nested_loops.f90, edge_loops.f90. All compile and run correctly.

---
+++ test_programs/do_while.f90
@@ -0,0 +1,9 @@
+program test_do_while
+    implicit none
+    integer :: x
+    x = 100
+    do while (x > 1)
+        x = x / 2
+    end do
+    print *, x
+end program
---
+++ test_programs/negative_step.f90
@@ -0,0 +1,7 @@
+program test_negative_step
+    implicit none
+    integer :: i
+    do i = 5, 1, -1
+        print *, i
+    end do
+end program
