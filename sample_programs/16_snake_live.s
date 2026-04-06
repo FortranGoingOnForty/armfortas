6046682099f474fafcb7effd7da836eed8dfaa14
mfwolffe <wolffemf@dukes.jmu.edu>
2026-04-06 19:45:39 -0400

    Expand live snake playfield

--- sample_programs/16_snake_live.s
+++ sample_programs/16_snake_live.s
@@ -377,18 +377,18 @@ init_game:
 
     adrp x11, _snake_x@PAGE
     add x11, x11, _snake_x@PAGEOFF
-    mov x10, #4
+    mov x10, #15
     str x10, [x11, #0]
-    mov x10, #3
+    mov x10, #14
     str x10, [x11, #8]
-    mov x10, #2
+    mov x10, #13
     str x10, [x11, #16]
-    mov x10, #1
+    mov x10, #12
     str x10, [x11, #24]
 
     adrp x12, _snake_y@PAGE
     add x12, x12, _snake_y@PAGEOFF
-    mov x10, #5
+    mov x10, #9
     str x10, [x12, #0]
     str x10, [x12, #8]
     str x10, [x12, #16]
@@ -590,11 +590,11 @@ advance_game:
 
     cmp x25, #0
     b.lt .Llose_now
-    cmp x25, #18
+    cmp x25, #30
     b.ge .Llose_now
     cmp x26, #0
     b.lt .Llose_now
-    cmp x26, #12
+    cmp x26, #18
     b.ge .Llose_now
 
     mov x27, #0
@@ -747,7 +747,7 @@ draw_frame:
 
     adrp x0, _border_line@PAGE
     add x0, x0, _border_line@PAGEOFF
-    mov x1, #20
+    mov x1, #32
     bl write_stdout
 
     adrp x0, _crlf@PAGE
@@ -758,7 +758,7 @@ draw_frame:
     mov x19, #0
 
 .Lrow_loop:
-    cmp x19, #12
+    cmp x19, #18
     b.ge .Lrows_done
 
     mov x0, #124
@@ -767,7 +767,7 @@ draw_frame:
     mov x20, #0
 
 .Lcol_loop:
-    cmp x20, #18
+    cmp x20, #30
     b.ge .Lrow_end
 
     mov x0, x20
@@ -793,7 +793,7 @@ draw_frame:
 .Lrows_done:
     adrp x0, _border_line@PAGE
     add x0, x0, _border_line@PAGEOFF
-    mov x1, #20
+    mov x1, #32
     bl write_stdout
 
     adrp x0, _crlf@PAGE
@@ -905,7 +905,7 @@ _quit_fmt:
     .asciz "Quit. Final score: "
 
 _border_line:
-    .asciz "+------------------+"
+    .asciz "+------------------------------+"
 
 _controls_line:
     .asciz "Controls: W A S D start/move, q quits"
@@ -939,14 +939,14 @@ _crlf:
     .byte 10
 
 _food_table:
-    .quad 12, 5
-    .quad 12, 8
-    .quad 6, 8
-    .quad 6, 2
-    .quad 15, 2
-    .quad 15, 10
-    .quad 3, 10
-    .quad 3, 3
+    .quad 22, 9
+    .quad 22, 13
+    .quad 10, 13
+    .quad 10, 4
+    .quad 26, 4
+    .quad 26, 15
+    .quad 5, 15
+    .quad 5, 5
 
 .section __DATA,__bss
 .p2align 3
