be20f75f556aed7677cea7c2e55470e4d0a568a9
espadonne <espadonne@outlook.com>
2026-07-04 02:41:34 -0400

    Repair inquire unit pos ABI

--- src/ir/lower/stmt.rs
+++ src/ir/lower/stmt.rs
@@ -7078,7 +7078,6 @@ pub(crate) fn lower_stmt(b: &mut FuncBuilder, ctx: &mut LowerCtx, stmt: &Spanned
                         unformatted_len,
                         leading_zero_ptr,
                         leading_zero_len,
-                        pos_addr,
                     ],
                     IrType::Void,
                 );
