7ec477193264459ad8de1a6bc5e13ff2c7ac0977
mfwolffe <mfwolffe@outlook.com>
2026-07-08 17:24:14 -0400

    Cover derived reverse sections

--- test_programs/ar8_overlap_section_derived_char.f90
+++ test_programs/ar8_overlap_section_derived_char.f90
@@ -1,5 +1,6 @@
 ! CHECK: derf= 1 1 2 3 4
 ! CHECK: derr= 2 3 4 5 5
+! CHECK: derrev= 5 4 3 2 1
 ! CHECK: charf= a1 a1 b2 c3 d4
 ! CHECK: charr= b2 c3 d4 e5 e5
 ! CHECK: ok
@@ -28,16 +29,22 @@ program ar8_overlap_section_derived_char
   if (p(4)%n /= 5 .or. p(5)%n /= 5) error stop 4
   print '(a,5(1x,i0))', 'derr=', p(1)%n, p(2)%n, p(3)%n, p(4)%n, p(5)%n
 
+  call init_cells(p)
+  p = p(5:1:-1)
+  if (p(1)%n /= 5 .or. p(2)%n /= 4 .or. p(3)%n /= 3) error stop 5
+  if (p(4)%n /= 2 .or. p(5)%n /= 1) error stop 6
+  print '(a,5(1x,i0))', 'derrev=', p(1)%n, p(2)%n, p(3)%n, p(4)%n, p(5)%n
+
   c = ['a1', 'b2', 'c3', 'd4', 'e5']
   c(2:5) = c(1:4)
-  if (c(1) /= 'a1' .or. c(2) /= 'a1' .or. c(3) /= 'b2') error stop 5
-  if (c(4) /= 'c3' .or. c(5) /= 'd4') error stop 6
+  if (c(1) /= 'a1' .or. c(2) /= 'a1' .or. c(3) /= 'b2') error stop 7
+  if (c(4) /= 'c3' .or. c(5) /= 'd4') error stop 8
   print '(a,5(1x,a))', 'charf=', c(1), c(2), c(3), c(4), c(5)
 
   c = ['a1', 'b2', 'c3', 'd4', 'e5']
   c(1:4) = c(2:5)
-  if (c(1) /= 'b2' .or. c(2) /= 'c3' .or. c(3) /= 'd4') error stop 7
-  if (c(4) /= 'e5' .or. c(5) /= 'e5') error stop 8
+  if (c(1) /= 'b2' .or. c(2) /= 'c3' .or. c(3) /= 'd4') error stop 9
+  if (c(4) /= 'e5' .or. c(5) /= 'e5') error stop 10
   print '(a,5(1x,a))', 'charr=', c(1), c(2), c(3), c(4), c(5)
 
   print '(a)', 'ok'
