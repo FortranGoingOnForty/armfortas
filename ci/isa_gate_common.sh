e7d51b72ad8a97793602a4d3f68486627be97008
mfwolffe <wolffemf@dukes.jmu.edu>
2026-09-03 22:14:34 -0400

    refresh compiler corpus audit counts

--- ci/isa_gate_common.sh
+++ ci/isa_gate_common.sh
@@ -3,7 +3,7 @@
 #
 # The counts are deliberate evidence contracts. Update them in the same change
 # as any test_programs/*.f90 addition/removal or ERROR_EXPECTED reclassification.
-isa_gate_expected_sources=882
+isa_gate_expected_sources=883
 isa_gate_expected_diagnostics=125
 
 isa_gate_validate_assembly() {
